fbda1d24b4b866dabdb130f11f96117cb4727862
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2024-11-12 13:56:36 +0000

    1.20

--- typescript-sdk/dist/main.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './sdk';
-export * from './generated-client/apicurioRegistryClient';
--- typescript-sdk/dist/sdk/factory.d.ts
+++
@@ -1,5 +0,0 @@
-import { AuthenticationProvider } from '@microsoft/kiota-abstractions';
-import { ApicurioRegistryClient } from '../generated-client/apicurioRegistryClient.ts';
-export declare class RegistryClientFactory {
-    static createRegistryClient(baseUrl: string, authProvider?: AuthenticationProvider): ApicurioRegistryClient;
-}
